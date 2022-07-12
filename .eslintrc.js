ec1f73700b42340e0eede1c66c1030f134cd9a00
Michael Novotny <manovotny@gmail.com>
2022-07-12 16:47:57 -0500

    Adds additional `react-hooks/exhaustive-deps` link / reference.

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,6 +4,7 @@ module.exports = {
     rules: {
         // Fixed in next version of GOML
         '@typescript-eslint/no-floating-promises': 'off',
+        // https://github.com/vercel/next.js/discussions/29403#discussioncomment-1908563
         // https://github.com/facebook/react/issues/16873#issuecomment-536346885
         'react-hooks/exhaustive-deps': 'off',
     },
